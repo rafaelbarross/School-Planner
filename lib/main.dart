1402cd5ceb07356f029a42c0a9ea781c0d468457
Rafael Barros <rsb8@aluno.ifal.edu.br>
2023-06-11 20:24:22 -0300

    Add navegação de telas

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:school_planner/components/show_dialog/show_dialog.dart';
-import 'materias.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'materia.dart';
+import '../screens/materia.dart';
+import '../components/listOfMaterias/list_materia.dart';
 
 void main() {
   runApp(MaterialApp(home: App()));
@@ -19,61 +21,76 @@ class _App extends State<App> {
   @override
   Widget build(BuildContext context) {
     // String texto = "Olá";
-    return Scaffold(
-      // HEADER APP BAR
-      appBar: AppBar(
-        backgroundColor: Color(0xFF00A4BA),
-        title: Row(children: [
-          Image.asset('lib/assets/icons/logo.png'),
-          // Padding(padding: padding)
-          SizedBox(width: 5),
-          Text(
-            "School Planner",
-            style: GoogleFonts.inter(
-              fontSize: 29,
-              fontWeight: FontWeight.bold,
-              color: Color(0xFF292D32),
+    return MaterialApp(
+      home: Scaffold(
+        // HEADER APP BAR
+        appBar: AppBar(
+          // toolbarHeight: 86,
+          backgroundColor: const Color(0xFF00A4BA),
+          title: Row(children: [
+            Image.asset('lib/assets/icons/SchoolPlanner.png'),
+            // Padding(padding: padding)
+            const SizedBox(width: 10),
+            Text(
+              "School Planner",
+              style: GoogleFonts.inter(
+                fontSize: 29,
+                color: const Color(0xFF292D32),
+              ),
+            ),
+          ]),
+          centerTitle: true,
+          iconTheme: const IconThemeData(
+            color: Color(0xFF292D32),
+            size: 36,
+          ),
+          actions: [
+            IconButton(
+              alignment: Alignment.centerRight,
+              icon: Image.asset('lib/assets/icons/menu-home.png'),
+              color: const Color(0xFF292D32),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                ListMateria(),
+                const SizedBox(height: 15),
+              ],
             ),
           ),
-        ]),
-        centerTitle: true,
-        iconTheme: IconThemeData(
-          color: Color(0xFF292D32),
-          size: 36,
         ),
-        actions: [
-          IconButton(
-            icon: Image.asset('lib/assets/icons/menu-home.png'),
-            color: Color(0xFF292D32),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      body: Center(
-        child: Text(
-          "Add stories to \nthe button",
-          style: TextStyle(
-            fontSize: 29,
-            color: Color.fromRGBO(0, 0, 0, 0.25),
-          ),
-        ),
-      ),
-      floatingActionButton: Container(
-        height: 80,
-        width: 80,
-        // margin: EdgeInsets.only(bottom: 100, right: 40),
-        // padding: EdgeInsets.all(25),
-        child: FloatingActionButton(
-          backgroundColor: Color(0xFF292D32),
-          child: Image.asset('lib/assets/icons/button-floating.png'),
-          onPressed: () {
-            showDialog(
-              context: context,
+
+        // body: Center(
+        //   child: Text(
+        //     "Adicione matérias\nno botão",
+        //     style: TextStyle(
+        //       fontSize: 29,
+        //       color: Color.fromRGBO(0, 0, 0, 0.25),
+        //     ),
+        //   ),
+        // ),
+        floatingActionButton: Container(
+          height: 65,
+          width: 65,
+          // margin: EdgeInsets.only(bottom: 100, right: 40),
+          // padding: EdgeInsets.all(25),
+          child: FloatingActionButton(
+            backgroundColor: Color(0xFF292D32),
+            child: Image.asset('lib/assets/icons/button-floating.png'),
+            onPressed: () {
+              showDialog(
+                context: context,
                 builder: (BuildContext context) {
-                   return CustomDialog();
-              },
-);
-          },
+                  return CustomDialog();
+                },
+              );
+            },
+          ),
         ),
       ),
     );
