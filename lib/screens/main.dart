819862cd897af24f2fd2b3381a035774135b0d5f
Rafael Barros <rsb8@aluno.ifal.edu.br>
2023-06-11 18:19:47 -0300

    Retornando para o estado da branch main

--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
-import 'materia.dart';
-import '../screens/materia.dart';
-import '../components/listOfMaterias/list_materia.dart';
+import 'package:school_planner/components/show_dialog/show_dialog.dart';
+import 'materias.dart';
 
 void main() {
-  runApp(const MaterialApp(home: App()));
+  runApp(MaterialApp(home: App()));
 }
 
 class App extends StatefulWidget {
@@ -21,80 +19,61 @@ class _App extends State<App> {
   @override
   Widget build(BuildContext context) {
     // String texto = "Olá";
-    return MaterialApp(
-      home: Scaffold(
-        // HEADER APP BAR
-        appBar: AppBar(
-          // toolbarHeight: 86,
-          backgroundColor: const Color(0xFF00A4BA),
-          title: Row(
-            children: [
-            Image.asset('lib/assets/icons/SchoolPlanner.png'),
-            // Padding(padding: padding)
-            const SizedBox(width: 10),
-            Text(
-              "School Planner",
-              style: GoogleFonts.inter(
-                fontSize: 29,
-                
-                color: const Color(0xFF292D32),
-              ),
+    return Scaffold(
+      // HEADER APP BAR
+      appBar: AppBar(
+        backgroundColor: Color(0xFF00A4BA),
+        title: Row(children: [
+          Image.asset('lib/assets/icons/logo.png'),
+          // Padding(padding: padding)
+          SizedBox(width: 5),
+          Text(
+            "School Planner",
+            style: GoogleFonts.inter(
+              fontSize: 29,
+              fontWeight: FontWeight.bold,
+              color: Color(0xFF292D32),
             ),
-          ]),
-          centerTitle: true,
-          iconTheme: const IconThemeData(
+          ),
+        ]),
+        centerTitle: true,
+        iconTheme: IconThemeData(
+          color: Color(0xFF292D32),
+          size: 36,
+        ),
+        actions: [
+          IconButton(
+            icon: Image.asset('lib/assets/icons/menu-home.png'),
             color: Color(0xFF292D32),
-            size: 36,
+            onPressed: () {},
           ),
-          actions: [
-            IconButton(
-              alignment: Alignment.centerRight,
-              icon: Image.asset('lib/assets/icons/menu-home.png'),
-              color: const Color(0xFF292D32),
-              onPressed: () {},
-            ),
-          ],
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(20),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                ListMateria(),
-                const SizedBox(height: 15),
-              
-                
-              ],
-            ),
+        ],
+      ),
+      body: Center(
+        child: Text(
+          "Add stories to \nthe button",
+          style: TextStyle(
+            fontSize: 29,
+            color: Color.fromRGBO(0, 0, 0, 0.25),
           ),
         ),
-
-        // body: Center(
-        //   child: Text(
-        //     "Adicione matérias\nno botão",
-        //     style: TextStyle(
-        //       fontSize: 29,
-        //       color: Color.fromRGBO(0, 0, 0, 0.25),
-        //     ),
-        //   ),
-        // ),
-        floatingActionButton: Container(
-          height: 65,
-          width: 65,
-          // margin: EdgeInsets.only(bottom: 100, right: 40),
-          // padding: EdgeInsets.all(25),
-          child: FloatingActionButton(
-            backgroundColor: Color(0xFF292D32),
-            child: Image.asset('lib/assets/icons/button-floating.png'),
-        
-            onPressed: () {
-              // Navigator.of(context).push(
-              //   MaterialPageRoute(
-              //     builder: (context) => _selectCategory(context),
-              //   ),
-              // );
-            },
-          ),
+      ),
+      floatingActionButton: Container(
+        height: 80,
+        width: 80,
+        // margin: EdgeInsets.only(bottom: 100, right: 40),
+        // padding: EdgeInsets.all(25),
+        child: FloatingActionButton(
+          backgroundColor: Color(0xFF292D32),
+          child: Image.asset('lib/assets/icons/button-floating.png'),
+          onPressed: () {
+            showDialog(
+              context: context,
+                builder: (BuildContext context) {
+                   return CustomDialog();
+              },
+);
+          },
         ),
       ),
     );
